78e0f4a6178f2e6c912320c67d861e1c7bd9c3aa
saviosabino <saviopp@gmail.com>
2014-01-22 13:30:08 -0200

    updates to run in django 1.6

---
+++ project_sample/project_sample/urls.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+#from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import include, patterns, url
+#from django.views.generic.simple import direct_to_template
+from django.views.generic.base import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="homepage.html")),
+    (r'^schedule/', include('schedule.urls', namespace="schedule")),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/(.*)', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
+
